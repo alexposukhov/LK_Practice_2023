31dc07d6bdc81467cf66c718fd18d470e532f67c
Oleksandr Posukhov <oleksandr.posukhov@globallogic.com>
2023-01-13 12:43:00 +0200

    LK_Practice: Add Simple Module example
    Added Simple module example and Makefile example

---
+++ 01_Simple_Module/simple_mod.c
@@ -0,0 +1,24 @@
+#include <linux/module.h>
+#include <linux/init.h>
+
+/**
+ * @brief Func is called when module is loaded
+ */
+static int __init modInit(void) {
+    printk("Simple module has started\n");
+    return 0;
+}
+
+/**
+ * @brief Func is called when module removed
+ */
+static void __exit modExit(void) {
+    printk("Simple module has removed\n");
+}
+
+module_init(modInit);
+module_exit(modExit);
+/*Module information*/
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("ME");
+MODULE_DESCRIPTION("Just a simple module");
